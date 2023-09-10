5adea053a377f4b70eba86e11d52ea55c85ab5f8
Liang <72845312+Liang417@users.noreply.github.com>
2023-09-10 20:19:36 +0800

    Add: Week-2_Assignment-5
    - Complete Week-2_Assignment-5 requirements
    - Develop necessary features

---
+++ Week-2/Assignment-5/main.js
@@ -0,0 +1,19 @@
+// TC:O(nlogn), SC:O(1)
+function binarySearchPosition(numbers, target) {
+  let l = 0;
+  let r = numbers.length - 1;
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+    if (numbers[mid] === target) {
+      return mid;
+    } else if (numbers[mid] > target) {
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 1)); // 0
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 6)); // 3
